ea0f5dbe575724f65c9b5ced503e882e560c0e73
AnthonyChenevierEDU <achenevier@kangan.edu.au>
2024-08-01 15:00:29 +1000

    update pyscript

--- Web/main.py
+++ Web/main.py
@@ -10,15 +10,20 @@ def dice_roll(faces):
     face_value = random.choice(face_range)
     return "Rolled a " + str(face_value)
 
+selected_faces = "Coin"
+def change_handler(event = None):
+    global selected_faces
+    if event:
+        selected_faces = event.target.value
+        print("BA" . selected_faces)
 
 def roll(event):
-    dc = pydom["input#dice-count"].value
-    # faces = pydom["select#dice-faces"].value
+    dc = pydom["input#dice-count"][0].value
     print(dc)
-    # print(faces)
-    #for i in range(dc):
-     #   if faces == "Coin":
-      #      faces = "D2"
-#
- #       current_roll = dice_roll(int(faces[1:]))
-  #      pydom["p#output"].content = current_roll
+    print(selected_faces)
+    for _ in range(dc):
+        if faces == "Coin":
+            faces = "D2"
+
+    current_roll = dice_roll(int(faces[1:]))
+    pydom["p#output"].content = current_roll
