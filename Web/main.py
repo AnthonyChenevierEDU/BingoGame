5f3eba4236ca3e52e4d653fb3992fc79d3c1656c
Anthony Chenevier <anthony.chenevier@gmail.com>
2024-08-05 13:43:39 +1000

    Final version solution

--- Web/main.py
+++ Web/main.py
@@ -1,43 +1,32 @@
-import random  # import the random library module
 from pyscript import document
 import dice
 
 
-# GLOBAL (program-wide) variable
-# this list holds the selections and
-# counts made for each dice type
-# each entry in the list is a tuple
-# containing number of dice to roll for that type
-# key = dice type
-# val - dice count
-# i.e. dice_selection[0] = ( "D6", 20 )
-dice_selection = []
-
-def add_dice(type_count):
-    elem = document.querySelector("div#dice-" + type_count)
-    elem.
+# GLOBAL (script-wide) variable
+# this stores the selected face option from the drop-down list
+dice_type = "Coin"
 
 
-def select_option(event=None):
-    if event:
-        t_id = event.target.id
-        print(t_id)
-
-        #selected_faces[t_id] = event.target.value
-        #print("Changed faces to " + selected_faces[t_id])
+def select_face_option(event):
+    global dice_type  # use global dice_type
+    dice_type = event.target.value
+    # print a debugging statement to the console
+    print("Changed dice_type to " + dice_type)
 
 
-def roll(event):
-    global selected_faces
-    dc = int(document.querySelector("input#dice-count-" + selected_faces).value)
-    rolls = []
-    faces = 0
-    for i in range(dc):
-        if selected_faces == "Coin":
-            faces = 2
-        else:
-            faces = selected_faces[1:]
-        rolls.append("(D" + faces + ") Roll " + str(i+1) + ": " + dice.dice_roll(faces) + "<br/>")
+def roll_all_dice(event):
+    global dice_type
+    if dice_type == "Coin":
+        faces = 2
+    else:
+        faces = int(dice_type[1:])
 
-    output = document.querySelector("p#output")
-    output.innerHTML = rolls
+    for roll in range(int(document.querySelector("input#dice-count").value)):
+        result = f"<p>(D{str(faces)}) Roll {str(roll + 1)}: {dice.dice_roll(faces)}</p>"
+        document.querySelector("div#roll-history").innerHTML += result
+
+    document.querySelector("div#roll-history").innerHTML += "<br />"
+
+
+def clear_history(event):
+    document.querySelector("div#roll-history").innerHTML = ""
